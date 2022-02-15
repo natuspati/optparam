86e7bad60a5dbc56310d479cbfb6b14b52f49317
Nurlat <natuspati@gmail.com>
2022-02-15 20:28:54 +0300

    added photos from lumix

--- mono.py
+++ mono.py
@@ -16,57 +16,57 @@ if __name__ == '__main__':
     target_pattern = Checkerboard(8, 11, 15)
     objp = target_pattern.gridpoints
 
-    path = "testimgs9"
-    ext = "*.JPG"
+    path = "testimgs7/dngs"
+    ext = "*.DNG"
 
     lst = list(map(str, list(Path(path).glob(ext))))
     objpoints = []
     imgpoints = []
 
-    # for raw_img in lst:
-    #     with rawpy.imread(raw_img) as raw:
-    #         rgb = raw.postprocess()
-    #         gray = cv.cvtColor(rgb, cv.COLOR_BGR2GRAY)
-    #         # Find the chess board corners
-    #         ret, corners = cv.findChessboardCorners(gray, (7, 10), None)
-    #         # If found, add object points, image points (after refining them)
-    #         if ret:
-    #             objpoints.append(objp)
-    #             corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-    #             imgpoints.append(corners)
-
     for raw_img in lst:
-        img = cv.imread(raw_img)
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, (7, 10), None)
-        # If found, add object points, image points (after refining them)
-        if ret:
-            objpoints.append(objp)
-            corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-            imgpoints.append(corners)
+        with rawpy.imread(raw_img) as raw:
+            rgb = raw.postprocess()
+            gray = cv.cvtColor(rgb, cv.COLOR_BGR2GRAY)
+            # Find the chess board corners
+            ret, corners = cv.findChessboardCorners(gray, (7, 10), None)
+            # If found, add object points, image points (after refining them)
+            if ret:
+                objpoints.append(objp)
+                corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+                imgpoints.append(corners)
+
+    # for raw_img in lst:
+    #     img = cv.imread(raw_img)
+    #     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    #     # Find the chess board corners
+    #     ret, corners = cv.findChessboardCorners(gray, (7, 10), None)
+    #     # If found, add object points, image points (after refining them)
+    #     if ret:
+    #         objpoints.append(objp)
+    #         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+    #         imgpoints.append(corners)
 
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-    # with rawpy.imread(lst[0]) as raw:
-    #     rgb = raw.postprocess()
-    #     h, w = rgb.shape[:2]
-    #     newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    with rawpy.imread(lst[0]) as raw:
+        rgb = raw.postprocess()
+        h, w = rgb.shape[:2]
+        newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+        # undistort
+        dst = cv.undistort(rgb, mtx, dist, None, newcameramtx)
+        # # crop the image
+        # x, y, w, h = roi
+        # dst = dst[y:y + h, x:x + w]
+        cv.imwrite('phoneraw.png', dst)
+
+    # rgb = cv.imread(lst[0])
+    # h, w = rgb.shape[:2]
+    # newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
     #
-    #     # undistort
-    #     dst = cv.undistort(rgb, mtx, dist, None, newcameramtx)
-    #     # # crop the image
-    #     # x, y, w, h = roi
-    #     # dst = dst[y:y + h, x:x + w]
-    #     cv.imwrite('calibresultlumix.png', dst)
-
-    rgb = cv.imread(lst[0])
-    h, w = rgb.shape[:2]
-    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-
-    # undistort
-    dst = cv.undistort(rgb, mtx, dist, None, newcameramtx)
-    cv.imwrite('calibresultjpg.png', dst)
+    # # undistort
+    # dst = cv.undistort(rgb, mtx, dist, None, newcameramtx)
+    # cv.imwrite('calibresultjpg.png', dst)
 
     # imgcon = ImageContainer("testimgs6/dngs", "*.DNG")
     # img_size = imgcon.imgsize
