a113e67b2e398f628cc5a1f84f7ef071b731a198
Nurlat <natuspati@gmail.com>
2022-08-24 18:01:53 +0300

    Preparing for clean merge with main from comparison

--- testimgs11/lensdist.m
+++
@@ -1,5 +0,0 @@
-images = dir('2d/*.tiff'); 
-patternDims = [4, 7];
-img = imread(strcat('2d/', images(1).name));
-grayimg = rgb2gray(img);
-imagePoints = detectCircleGridPoints(grayimg,patternDims,PatternType="asymmetric");
